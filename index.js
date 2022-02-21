e3ea9875c263d6a384de341e5c217ab5667692e6
Diaa M. Shalabi <30384731+diaa-shalabi@users.noreply.github.com>
2022-02-21 13:17:54 +0000

    commit

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('Hello World');
+// The Rest parameters
+
+function sum(){
+  let total = 0;
+  for(let value of arguments){
+    total+=value;
+  }
+  return total;
+}
+
+function sum1(...args){
+  return args.reduce((a,b)=>a+b);
+}
+
+function sum2(discount, ...prices){
+  let total = prices.reduce((a,b)=>a+b);
+  return total * (1 - discount);
+}
+
+console.log(sum(1, 8, 9 ,6));
+console.log(sum1(1, 8, 9 ,6));
+console.log(sum2(0.1, 20,30 ));
+
+// setter and getter
+
+// getters => access properties
+// seters => change (mutate) them
+
+const person = {
+  firstName:'Diaa',
+  lastName:'Shalabi',
+  get fullName(){
+    return `${person.firstName} ${person.lastName}`;
+  },
+  set fullName(value){
+    const parts = value.split(' ');
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  }
+};
+
+person.fullName = "John Smith";
+console.log(person.fullName);
+
+//
